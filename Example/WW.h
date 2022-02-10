aa6455505a13e0d35c7370b12c2e1188bd3dcc2e
wang bowen <sysworker@163.com>
2022-02-10 17:51:03 +0800

    测试

---
+++ Example/WW.h
@@ -0,0 +1,17 @@
+//
+//  WW.h
+//  WWBanner_Example
+//
+//  Created by wang bowen on 2022/2/10.
+//  Copyright © 2022 sysworker@163.com. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface WW : UIView
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ Example/WW.m
@@ -0,0 +1,21 @@
+//
+//  WW.m
+//  WWBanner_Example
+//
+//  Created by wang bowen on 2022/2/10.
+//  Copyright © 2022 sysworker@163.com. All rights reserved.
+//
+
+#import "WW.h"
+
+@implementation WW
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
